f3d9db42cdfd148b2c49d6ba012925dcb4e7b36b
Italo-13 <italo.peredar@alu.ucm.cl>
2023-04-11 01:17:00 -0400

    creacion de la funcion euclidiana + valores entregados por ella en el archivo de prueba uno.c

--- tarea_2.c
+++ tarea_2.c
@@ -2,28 +2,25 @@
 #include<stdlib.h>
 #include <omp.h>
 #include <time.h> /* Nueva librería necesaria para la función srand */
+#define pass (void)0
 #define DIM 20
 #define T 8
 #define K 3
 //crear una funcion que devuelva el puntero del arreglo con los centroides
-
-int **NumneroAleatorios(int N_DB){
+//generamos numeros aleatorios de 0 a n_db(cantidad de filas del archivo txt), luego retornamos en un arreglo los valores
+int *NumneroAleatorios(int N_DB){
     time_t t;
-    int N=2;
-    int i, j,a,b;
-    int **aleatorio;
-    aleatorio = (int **)malloc(sizeof(int *)*K);
-	for (int i=0; i < K; i++){
-        aleatorio[i] = (int *)malloc(sizeof(int)*N);   
-    }
+    int i, j;
+    int *aleatorio;
+    aleatorio = (int*)malloc(K*sizeof(int));
     srand((unsigned) time(&t));
     for( i = 0 ; i < K ; i++ ) {
-        aleatorio[i][0]=rand() % N_DB;
-        aleatorio[i][1]=rand() % 20; 
+        aleatorio[i]=rand() % N_DB;
     }
     return aleatorio;
 }
-void lecturaDeMatriz(int filas,int columnas, int **m){
+//mostramos los valores de la matriz anidada
+void lecturaDeMatrizAnidada(int filas,int columnas, int **m){
     for(int i = 0; i < filas; i++){//fila
         for(int j = 0; j < columnas; j++){//columna
             printf("%i ", m[i][j]);
@@ -32,21 +29,45 @@ void lecturaDeMatriz(int filas,int columnas, int **m){
     }
     free(m);//liberamos memoria dinamica
 }
-float *BusquedaCentroide(int **aleatorio,float **ELEMS){
-    int N=2;
-    int a,b,i,j;
-    float *centroides;
-    centroides=(float *) malloc(K*sizeof(float));
+//mostramos los valores de la matriz
+void lecturaDeMatriz(int *m){
+    int i;
+    for (i=0;i<K;i++){
+        printf("%i\n",m[i]);
+    }
+}
+//buscamos los centroides dentro de la base de datos
+float **BusquedaCentroide(int *aleatorio,float **ELEMS){
+    int a,i;
+    float **centroides;
+    centroides=(float **)malloc(sizeof(float *)*K);
     for (i=0; i < K; i++)
 	{
-		for (j=0; j < N; j++)
-			if (j == 0)
-                a=aleatorio[i][j];
-			else
-                b=aleatorio[i][j];
-            centroides[i]=ELEMS[a][b];
+        a=aleatorio[i];
+        centroides[i]=ELEMS[a];
 	}
     return centroides;
+}
+int *Euclidiana(int *aleatorio,float **ELEMS,int N_DB){
+    int a,i,j,k;
+    float  *memoria[K];
+    for (i=0; i < K; i++)
+	{
+        a=aleatorio[i];
+        memoria[i]= (float*)&ELEMS[a];//copiamos la direccion de memoria
+	}
+    memoria;
+    int resultado;
+    for(i=0;i<N_DB;i++){
+        for(j=0;j<K;j++){
+            if(memoria[j]==(float*)&ELEMS[i]){
+                pass;
+            }
+            else
+              
+        }
+    }
+    return 0;
 }
 int main()
 {
@@ -61,26 +82,14 @@ int main()
 	for (i=0; i < N_DB; i++)
 		for (j=0; j < DIM; j++)
 			scanf("%f", &(ELEMS[i][j]));
-
 	printf("%d\n", N_DB);//tamaño de base de datos
-    int **matriz_aleatoria=NumneroAleatorios(N_DB);
-    //lecturaDeMatriz(3,2,matriz_aleatoria);
-    float *centroides=BusquedaCentroide(matriz_aleatoria,ELEMS);
-    /*for ( i = 0; i < K; i++)
-    {
-        printf("%f\n",centroides[i]);
-    }*/
-    for (i=0; i < N_DB; i++)
-
-	{
-        float x,y,resultado;
-		for (j=0; j < DIM; j++)
-        {
-			y=ELEMS[i][j];
-            printf("%f\n",y);
-        }
-        printf("---------");//calclar distancia, y son los elemntos y x van a ser los 3 cluster creo que debe ir un if
-	}
+    int *matriz_aleatoria=NumneroAleatorios(N_DB);
+    float **centroides;
+    int *euclidiana;
+    centroides=BusquedaCentroide(matriz_aleatoria,ELEMS);
+    euclidiana=Euclidiana(matriz_aleatoria,ELEMS,N_DB);
+    //calclar distancia, y son los elemntos y x van a ser los 3 cluster creo que debe ir un if
+	
     //resultado 1 sqrt(y-x[0])^2 x cetroide
     //resultado 3 sqrt(y-x[2])^2
     //resultado 2 sqrt(y-x[1])^2
--- uno.c
+++ uno.c
@@ -1,47 +1,52 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include <time.h> /* Nueva librería necesaria para la función srand */
-
+#include<math.h>
 #define K 3
+#define pass (void)0
 
-int **crear_arreglo(){
-    int **matriz;
-    time_t t;
-    int N=2;
-    int a,b;
-    int **aleatorio;
-    aleatorio = (int **)malloc(sizeof(int *)*K);
-	for (int i=0; i < K; i++){
-        aleatorio[i] = (int *)malloc(sizeof(int)*N);
-        
-    }
-    srand((unsigned) time(&t));
-    for( int i = 0 ; i < K ; i++ ) {
-        aleatorio[i][0]=rand() % 100;
-        aleatorio[i][1]=rand() % 20; 
-    }
-		
-    
-    /*matriz = (int **) malloc(2 * sizeof(int *));
-    for(int i = 0; i < 2; i++){
-        matriz[i] = (int *) malloc(2 * sizeof(int));
-        for(int j = 0; j < 2; j++){
-            matriz[i][j] = i + j;
-        }
-    }*/
-    
-    printf("%i",aleatorio[0][0]);
-    return aleatorio;
-}
 int main(){
-    int **matriz = crear_arreglo();
+    float ELEMS[4][5] = { {10.0, 20, 30, 42.0, 50},{60.0,8.20, 35.0, 30, 50},{10.0, 20, 30, 80.0, 50},
+    {10.0, 12.0, 30, 4.0, 0.50}};
+    float centroides[2][5]={{60.0,8.20, 30, 30, 50},{10.0, 20, 30, 40, 50}};
+    int i,j,k;
     
-    for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 3; j++){
-            printf("%d ", matriz[i][j]);
+    for(i=0;i<4;i++){//cluster
+        float resultado=0;
+        int cluster=0;
+        float aux=0;
+        for(j=0;j<2;j++){
+            for(k=0;k<5;k++){
+                aux=(pow(centroides[j][k]-ELEMS[i][k],2))+aux;
+                printf("--%f--",aux);
+            }
+            aux=sqrt(aux);
+            printf("\n**%f**",aux); 
+            if (resultado==0){
+                resultado=aux;
+                cluster=j;
+            }else
+                if(resultado>aux){
+                    resultado=aux;    
+                    cluster=j; 
+                        
+                }
+                else
+                    continue;
+            
+            //se realcaculan los cluster 
+            //agrgar a que cluster pertence en una lista de lista             
         }
-        printf("\n");
-    }
+        printf("\nññññi el cluster es:%i",cluster);
+        printf("la distancia menor es_%f\n\n",resultado);
         
+
+
+        
+        
+    }
+    
+
+    return 0;   
 }
 
